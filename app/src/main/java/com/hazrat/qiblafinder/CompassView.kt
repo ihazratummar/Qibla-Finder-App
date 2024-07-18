25aaec09cdba819d5e1e82316787171a48f8b5f1
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2024-07-18 20:43:30 +0530

    design compass

--- app/src/main/java/com/hazrat/qiblafinder/CompassView.kt
+++ app/src/main/java/com/hazrat/qiblafinder/CompassView.kt
@@ -1,103 +1,112 @@
 package com.hazrat.qiblafinder
 
+import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Paint
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
-import android.graphics.Canvas
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.res.painterResource
-
-/**
- * @author Hazrat Ummar Shaikh
- */
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 
 @Composable
 fun CompassView(qiblaDirection: Float, currentDirection: Float) {
-    val compassBg = painterResource(id = R.drawable.compass_base)
-    val qiblaIcon = ImageBitmap.imageResource(id = R.drawable.qibla_icon)
+    val context = LocalContext.current
+    val compassBgBitmap = remember { drawableToBitmap(context, R.drawable.compass3).asImageBitmap() }
+    val qiblaIconBitmap = remember { drawableToBitmap(context, R.drawable.qibla_icon).asImageBitmap() }
+    val needleBitmap = remember { drawableToBitmap(context, R.drawable.needles).asImageBitmap() }
 
 
-    Column(
+    Column (
         modifier = Modifier
             .fillMaxSize()
-            .padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Icon(
             imageVector = Icons.Default.ArrowDropDown,
             contentDescription = null,
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .padding(10.dp)
+        ) {
+            // Down arrow icon
 
-        Box(modifier = Modifier.fillMaxHeight(0.5f)) {
-            // Custom compass background
-            Image(
-                painter = compassBg,
-                contentDescription = "Compass Background",
-                modifier = Modifier.fillMaxSize()
-            )
 
+            // Canvas for compass
             Canvas(modifier = Modifier.fillMaxSize()) {
                 val compassCenter = Offset(size.width / 2, size.height / 2)
                 val compassRadius = size.minDimension / 2.5f
 
-                // Draw the current direction needle
-                rotate(degrees = currentDirection, pivot = compassCenter) {
-                    drawLine(
-                        color = Color.Green,
-                        start = compassCenter,
-                        end = Offset(compassCenter.x, compassCenter.y - compassRadius),
-                        strokeWidth = 8f
+                // Rotate the entire compass background image
+                rotate(degrees = -currentDirection, pivot = compassCenter) {
+                    drawImage(
+                        image = compassBgBitmap,
+                        topLeft = Offset(
+                            compassCenter.x - compassBgBitmap.width / 2,
+                            compassCenter.y - compassBgBitmap.height / 2
+                        )
                     )
                 }
 
+                // Draw the current direction needle
+//                rotate(degrees = currentDirection, pivot = compassCenter) {
+//                    drawLine(
+//                        color = Color.Green,
+//                        start = compassCenter,
+//                        end = Offset(compassCenter.x, compassCenter.y - compassRadius),
+//                        strokeWidth = 8f
+//                    )
+//                }
+
                 // Draw the Qibla direction needle
                 rotate(degrees = qiblaDirection - currentDirection, pivot = compassCenter) {
-                    drawLine(
-                        color = Color.Red,
-                        start = compassCenter,
-                        end = Offset(compassCenter.x, compassCenter.y - compassRadius),
-                        strokeWidth = 8f
+                    drawImage(
+                        image = needleBitmap,
+                        topLeft = Offset(
+                            compassCenter.x - needleBitmap.width / 2,
+                            compassCenter.y - compassRadius + 110  - needleBitmap.height / 25
+                        )
                     )
 
                     // Draw the Qibla icon
                     drawImage(
-                        image = qiblaIcon,
+                        image = qiblaIconBitmap,
                         topLeft = Offset(
-                            compassCenter.x - qiblaIcon.width / 2,
-                            compassCenter.y - compassRadius - qiblaIcon.height / 2
+                            compassCenter.x - qiblaIconBitmap.width / 2,
+                            compassCenter.y - compassRadius - qiblaIconBitmap.height / 1
                         )
                     )
                 }
             }
         }
     }
-
 }
 
-
+fun drawableToBitmap(context: Context, drawableId: Int): Bitmap {
+    val drawable: Drawable = ContextCompat.getDrawable(context, drawableId)!!
+    return drawable.toBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight)
+}
--- app/src/main/java/com/hazrat/qiblafinder/MainActivity.kt
+++ app/src/main/java/com/hazrat/qiblafinder/MainActivity.kt
@@ -49,6 +49,7 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         registerSensorListeners()
     }
 
+    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)} passing\n      in a {@link RequestMultiplePermissions} object for the {@link ActivityResultContract} and\n      handling the result in the {@link ActivityResultCallback#onActivityResult(Object) callback}.")
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_LOCATION_PERMISSION && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
