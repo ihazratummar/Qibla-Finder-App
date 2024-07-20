a15b60f38d0b44c4d27e27e9d2bc9234c21795e8
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2024-07-20 13:03:38 +0530

    Complete app

--- app/src/main/java/com/hazrat/qiblafinder/CompassView.kt
+++ app/src/main/java/com/hazrat/qiblafinder/CompassView.kt
@@ -1,8 +1,7 @@
 package com.hazrat.qiblafinder
 
-import androidx.compose.foundation.BorderStroke
+
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,9 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,28 +22,53 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.hazrat.qiblafinder.ui.theme.Hidaya
+import com.hazrat.qiblafinder.util.vibrateDevice
+
+/**
+ * @author Hazrat Ummar Shaikh
+ */
+
+
+/**
+ * QiblaScreen is a composable function that displays a compass indicating the Qibla direction.
+ * It also vibrates the device when the user faces the Qibla.
+ *
+ * @param modifier The modifier to be applied to the QiblaScreen.
+ * @param navController The NavHostController for navigation.
+ * @param qiblaDirection The direction of the Qibla in degrees.
+ * @param currentDirection The current direction of the device in degrees.
+ */
 
 @Composable
-fun CompassView(
+fun QiblaScreen(
     modifier: Modifier = Modifier,
-    qiblaDirection: Float, currentDirection: Float
+    qiblaDirection: Float,
+    currentDirection: Float,
 ) {
+
+
     val context = LocalContext.current
+
     val compassBgBitmap =
         remember { drawableToBitmap(context, R.drawable.compass3).asImageBitmap() }
     val qiblaIconBitmap =
-        remember { drawableToBitmap(context, R.drawable.qiblaicon).asImageBitmap() }
+        remember { drawableToBitmap(context, R.drawable.qiblaiconpoint).asImageBitmap() }
     val needleBitmap = remember { drawableToBitmap(context, R.drawable.needles).asImageBitmap() }
+    val goldQaba = remember { drawableToBitmap(context, R.drawable.goldqaba).asImageBitmap() }
 
-    val minTolerance = 3.1f // Adjusted tolerance range
-    val maxTolerance = 3.4f // Adjusted tolerance range
+    val minTolerance = 3f // Adjusted tolerance range
+    val maxTolerance = 3.5f // Adjusted tolerance range
 
     val directionDifference = qiblaDirection - currentDirection
     val normalizedDifference = (directionDifference + 360) % 360
@@ -57,107 +82,92 @@ fun CompassView(
 
     // Vibrate when facing Qibla and not already vibrated
     if (isFacingQibla && !hasVibrated) {
-        vibrateDevice(context)
+        vibrateDevice(context, 200)
         hasVibrated = true // Set to true to prevent continuous vibration
     } else if (!isFacingQibla) {
         hasVibrated = false // Reset when not facing Qibla
     }
 
-
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(30.dp)
+            .statusBarsPadding(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
     ) {
-        if (isFacingQibla) {
-            Card(
-                modifier = Modifier
-                    .padding(10.dp)
-                    .size(20.dp)
-                    .shadow(
-                        elevation = 5.dp,
-                        ambientColor = Color.Cyan,
-                        spotColor = Color.Cyan
-                    )
-                    .background(
-                        color = Color(0xff01716a),
-                        shape = CircleShape
-                    ),
-                shape = CircleShape,
-                border = BorderStroke(1.dp, color = Color(0xff01716a)),
-                colors = CardDefaults.cardColors(containerColor = Color.Transparent)
-            ) {
-
-            }
-        } else {
-            Card(
-                modifier = Modifier
-                    .padding(10.dp)
-                    .size(20.dp),
-                shape = CircleShape,
-                border = BorderStroke(1.dp, color = Color.Black),
-                colors = CardDefaults.cardColors(containerColor = Color.Transparent)
-            ) {
-
-            }
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-        Box(
+        Column(
             modifier = Modifier
-                .fillMaxHeight(0.5f)
-                .padding(10.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // Down arrow icon
 
+            Text(
+                text = "Qibla: ${qiblaDirection.toInt()}°",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = Hidaya
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            if (isFacingQibla) {
+                Icon(
+                    modifier = Modifier.size(60.dp),
+                    painter = painterResource(id = R.drawable.goldqaba),
+                    contentDescription = null,
+                    tint = Color.Unspecified
+                )
+            } else {
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.arrowup),
+                    contentDescription = null
+                )
+            }
 
-            // Canvas for compass
-            Canvas(modifier = Modifier.fillMaxSize()) {
-                val compassCenter = Offset(size.width / 2, size.height / 2)
-                val compassRadius = size.minDimension / 2.5f
+            Spacer(modifier = Modifier.height(10.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .padding(10.dp)
+            ) {
+                // Canvas for compass
+                Canvas(modifier = Modifier.fillMaxSize()) {
+                    val compassCenter = Offset(size.width / 2, size.height / 2)
+                    val compassRadius = size.minDimension / 2.5f
 
-                // Rotate the entire compass background image
-                rotate(degrees = -currentDirection, pivot = compassCenter) {
-                    drawImage(
-                        image = compassBgBitmap,
-                        topLeft = Offset(
-                            compassCenter.x - compassBgBitmap.width / 2,
-                            compassCenter.y - compassBgBitmap.height / 2
+                    // Rotate the entire compass background image
+                    rotate(degrees = -currentDirection, pivot = compassCenter) {
+                        drawImage(
+                            image = compassBgBitmap,
+                            topLeft = Offset(
+                                compassCenter.x - compassBgBitmap.width / 2,
+                                compassCenter.y - compassBgBitmap.height / 2
+                            )
                         )
-                    )
-                }
-
-                // Draw the current direction needle
-//                rotate(degrees = currentDirection, pivot = compassCenter) {
-//                    drawLine(
-//                        color = Color.Green,
-//                        start = compassCenter,
-//                        end = Offset(compassCenter.x, compassCenter.y - compassRadius),
-//                        strokeWidth = 8f
-//                    )
-//                }
-
-                // Draw the Qibla direction needle
-                rotate(degrees = qiblaDirection - currentDirection, pivot = compassCenter) {
-                    val needleStartY = compassCenter.y - needleBitmap.height / 1.1f
-                    drawImage(
-                        image = needleBitmap,
-                        topLeft = Offset(
-                            compassCenter.x - needleBitmap.width / 1.8f,
-                            needleStartY
+                    }
+                    // Draw the Qibla direction needle
+                    rotate(degrees = qiblaDirection - currentDirection, pivot = compassCenter) {
+                        val needleStartY = compassCenter.y - needleBitmap.height / 1.1f
+                        drawImage(
+                            image = needleBitmap,
+                            topLeft = Offset(
+                                compassCenter.x - needleBitmap.width / 2f,
+                                needleStartY
+                            )
                         )
-                    )
 
-                    // Draw the Qibla icon
-                    drawImage(
-                        image = qiblaIconBitmap,
-                        topLeft = Offset(
-                            compassCenter.x - qiblaIconBitmap.width / 2,
-                            compassCenter.y - compassRadius - qiblaIconBitmap.height / 1.1f
-                        ),
-                    )
+                        // Draw the Qibla icon
+                        if (!isFacingQibla) {
+                            drawImage(
+                                image = qiblaIconBitmap,
+                                topLeft = Offset(
+                                    compassCenter.x - qiblaIconBitmap.width / 2,
+                                    compassCenter.y - compassRadius - qiblaIconBitmap.height / 1.1f
+                                ),
+                            )
+                        }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/hazrat/qiblafinder/util/Utils.kt
@@ -0,0 +1,46 @@
+package com.hazrat.qiblafinder.util
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.VibrationEffect
+import android.os.Vibrator
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+
+/**
+ * @author Hazrat Ummar Shaikh
+ */
+
+fun calculateQiblaDirection(latitude: Double, longitude: Double): Double {
+    val kaabaLatitude = 21.4225
+    val kaabaLongitude = 39.8262
+
+    val latDifference = Math.toRadians(kaabaLatitude - latitude)
+    val lonDifference = Math.toRadians(kaabaLongitude - longitude)
+    val y = sin(lonDifference) * cos(Math.toRadians(kaabaLatitude))
+    val x = cos(Math.toRadians(latitude)) * sin(Math.toRadians(kaabaLatitude)) -
+            sin(Math.toRadians(latitude)) * cos(Math.toRadians(kaabaLatitude)) * cos(lonDifference)
+    return (Math.toDegrees(atan2(y, x)) + 360) % 360
+}
+
+
+fun drawableToBitmap(context: Context, drawableId: Int): Bitmap {
+    val drawable: Drawable = ContextCompat.getDrawable(context, drawableId)!!
+    return drawable.toBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight)
+}
+
+fun vibrateDevice(context: Context, virateTime: Long) {
+    val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
+    if (vibrator != null && vibrator.hasVibrator()) {
+        vibrator.vibrate(
+            VibrationEffect.createOneShot(
+                virateTime, // Vibration duration in milliseconds
+                VibrationEffect.DEFAULT_AMPLITUDE
+            )
+        )
+    }
+}
